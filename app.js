97fe57f98290a2e3422e2ed96d1f1302888e02ff
yogoken <yogo.k@husky.neu.edu>
2017-09-17 16:06:23 +0900

    Update both files to add vue.component

--- app.js
+++ app.js
@@ -5,20 +5,58 @@ function buildUrl(url) {
     return NYTBaseUrl + url + ".json?api-key=" + ApiKey
 }
 
-const vm = new Vue({
-  el: '#app',
-  data: {
-    results: []
-  },
-  mounted() {
-    this.getPosts('home');
-  },
-  methods: {
-    getPosts(section) {
-      let url = buildUrl(section)
-      axios.get(url).then((response) => {
-        this.results = response.data.results;
-      }).catch( error => { console.log(error); });
+
+Vue.component('news-list', {
+  props: ['results'],
+  template: `
+    <section>
+      <div class="row" v-for="posts in processedPosts">
+        <div class="columns medium-3" v-for="post in posts">
+          <div class="card">
+            <div class="card-divider">
+              {{ post.title }}
+            </div>
+            <a :href="post.url" target="_blank"><img :src="post.image_url"></a>
+            <div class="card-section">
+              <p>{{ post.abstract }}.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  `,
+    computed: {
+        processedPosts() {
+            let posts = this.results;
+            // Add image_url attribute
+            posts.map(post=>{
+                let imgObj = post.multimedia.find(media=>media.format==="superJumbo");
+            post.image_url = imgObj ? imgObj.url : "http://placehold.it/300x200?text=N/A";
+        });
+            // Put Array into Chunks
+            let i, j, chunkedArray = [], chunk = 4;
+            for (i=0, j=0; i < posts.length; i+=chunk, j++){
+                chunkedArray[j] = posts.slice(i, i+chunk);
+            }
+            return chunkedArray;
+        }
     }
-  }
+});
+
+const vm = new Vue({
+    el: '#app',
+    data: {
+        results: []
+    },
+    mounted() {
+        this.getPosts('home');
+    },
+    methods: {
+        getPosts(section) {
+            let url = buildUrl(section)
+            axios.get(url).then((response) => {
+                this.results = response.data.results;
+            }).catch( error => { console.log(error); });
+        }
+    }
 });
