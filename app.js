0f8da618fe2d9aeb11e658a4482f344b9183bb51
yogoken <yogo.k@husky.neu.edu>
2017-09-17 15:16:51 +0900

    Update .gitignore

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+const NYTBaseUrl="https://api.nytimes.com/svc/topstories/v2/";
+const ApiKey="aa1030639bd14935877fad15a36d1790";
+
+function buildUrl(url) {
+    return NYTBaseUrl + url + ".json?api-key=" + ApiKey
+}
+
 const vm = new Vue({
   el: '#app',
   data: {
     results: []
   },
   mounted() {
-    axios.get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=aa1030639bd14935877fad15a36d1790")
-    .then(response => {this.results = response.data.results})
+    this.getPosts('home');
+  },
+  methods: {
+    getPosts(section) {
+      let url = buildUrl(section)
+      axios.get(url).then((response) => {
+        this.results = response.data.results;
+      }).catch( error => { console.log(error); });
+    }
   }
 });
